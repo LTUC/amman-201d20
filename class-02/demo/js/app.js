894e351b8244b86787719b3b03a7eef3571b99c1
samer <s.allaham@ltuc.com>
2021-03-29 12:15:03 +0300

    pushing class2 demo

---
+++ class-02/demo/js/app.js
@@ -0,0 +1,172 @@
+'use strict';
+
+/*
+
+comments for one line:
+//
+
+comments for more than line:
+
+
+Data types:
+
+-String:
+any symplos inside 'single quotes' and "double quotes"
+
+-Numbers:
+2.5 float, 2 integer
+
+-Boolean:
+true or false
+
+*/
+
+
+// let userName='samer';
+
+// let age=prompt('what\'s your age');
+
+// // console.log(age);
+
+// // let number=55;
+// console.log('before',typeof age);
+
+// // age=parseInt(age);
+// age=Number(age);
+
+// console.log(age);
+// // console.log(typeof NaN);
+// console.log('after ',age,typeof age);
+
+
+/*
+If statements:
+
+if(condition){
+    do this code if the condition is true
+}else if(another condition){
+    do this code if the second condition is true
+}else{
+    do this if all conditions are false
+}
+
+
+*/
+
+// let feeling=confirm('are you good?');
+
+// if(feeling===true){
+//   console.log('thats great to hear');
+// }else{
+//   console.log('hope you feel better soon');
+// }
+
+// // alert('hellooo');
+
+// let favouriteColor=prompt('what do you like more red/black?');
+
+
+// favouriteColor=favouriteColor.toLowerCase();
+
+// // console.log(favouriteColor.toLowerCase());
+// console.log(favouriteColor);
+
+
+// if (favouriteColor==='black') {
+//   console.log('you like black more');
+// }else if (favouriteColor==='red') {
+//   console.log('you like red more');
+// }else{
+//   console.log('please write red or black');
+// }
+
+
+// // let test;
+
+// // console.log(test==undefined);
+// // console.log(typeof null);
+// // console.log(typeof undefined);
+// // if (test===null) {
+// // }
+
+// /*
+// Switch
+
+// switch(variable){
+//     case 'value1':
+//         do code...
+//         break;
+
+//     case 'value2'
+//         do code...
+//         break;
+
+//     default:
+//         do if nothing matches the casses
+//         break;
+// }
+
+
+// */
+
+// let fruit=prompt('what do you want?');
+
+
+// switch(fruit.toUpperCase()){
+//    case 'APPLE':
+//         console.log('apples are 5 dinars a kilo');
+//         break;
+//     case 'BANANA':
+//     case 'MELON':
+//         console.log('bananas and melons are 3 dinars a kilo');
+//         break;
+    
+//     default:
+//         console.log('sorry we dont have that ');
+//         break;
+// }
+
+
+/*
+And
+true && true => true
+true && false => false 
+false && true => false
+false && false=> false
+
+OR
+
+true || true => true
+true || false => true
+false || false => false
+*/
+
+
+let myBool=false;
+let anotherBool=false;
+let thirdBool=true;
+
+
+
+if (myBool && thirdBool) {
+    console.log('myBool and thirdBool are true');
+} else if(myBool|| anotherBool){
+    console.log('one of them was true');
+}else if(thirdBool){
+    console.log('third bool was true');
+}else{
+    console.log('is there any of them true?');
+}
+
+
+/*
+GIT:
+VCS
+LOCAL machine
+
+GitHub:
+the cloud
+store code and share it with other developers
+
+*/
+
